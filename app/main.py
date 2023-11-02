31c3af25f31880c91e06e5e8f7b33dbef2e68456
Pedro Dias <petermdias@gmail.com>
2023-11-02 14:35:07 +0000

    restai endpoints...

--- app/main.py
+++ app/main.py
@@ -2,6 +2,7 @@ import logging
 import os
 from tempfile import NamedTemporaryFile
 from fastapi import FastAPI, HTTPException, Request, UploadFile
+import requests
 
 from dotenv import load_dotenv
 from fastapi import  Form
@@ -18,4 +19,23 @@ def challenge(challenge: ChallengeModel):
 
 @app.post("/slash")
 def slash(channel_name: str = Form(...), user_name: str = Form(...), command: str = Form(...), text: str = Form(...)):
-    return {"channel_name": channel_name, "user_name": user_name, "command": command, "text": text}
+    words = text.split()
+    operation = words[0] if words else None
+    message = ' '.join(words[1:]) if len(words) > 1 else ''
+
+    if operation == 'ingest':
+        url = os.environ["RESTAI_URL"] + '/projects/' + os.environ["RESTAI_PROJECT"] + '/ingest/url'
+        data = {"url": message}
+        response = requests.post(url, json=data)
+        return response
+    elif operation == 'question':
+        url = os.environ["RESTAI_URL"] + '/projects/' + os.environ["RESTAI_PROJECT"] + '/question'
+        data = {"question": message}
+        response = requests.post(url, json=data)
+        answer = response.json().get('answer', '')
+        return answer
+    elif operation == 'chat':
+        pass
+    else:
+        return { "response_type": "in_channel", 'text': 'Please rewrite you statement.' }
+        pass
