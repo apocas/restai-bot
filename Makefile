dd55c024238e4d838d244dafb50e7d852d8cc694
Pedro Dias <petermdias@gmail.com>
2023-11-02 14:51:44 +0000

    make

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 .PHONY: start
 start:
-	uvicorn app.main:app --reload --port 9001
+	uvicorn app.main:app --reload --port 9001 --host 0.0.0.0
 
 install:
 	pip install -r requirements.txt
--- main.py
+++ main.py
@@ -1,7 +1,11 @@
+import os
 import uvicorn
 
 from app.main import app
 
+print("Starting server...")
+print(os.environ["RESTAI_URL"])
+print(os.environ["RESTAI_PROJECT"])
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=9000)
